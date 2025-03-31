90edc1eead9669c4c21afd37994ec00779ee2f26
Dawid <dawid.ku.info@gmail.com>
2025-03-31 22:15:38 +0200

    Usunięcie szybkiego wyszukiwania

--- PrzychodniaFX/src/main/java/pl/example/przychodniafx/MainController.java
+++ PrzychodniaFX/src/main/java/pl/example/przychodniafx/MainController.java
@@ -4,17 +4,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-//import pl.example.przychodniafx.controller.SearchUserController;
 
 public class MainController {
-
-    @FXML
-    private TextField first_nameField;
-
-    @FXML
-    private TextField last_nameField;
 
     @FXML
     private void handleAddUser() {
@@ -24,29 +16,6 @@ public class MainController {
     @FXML
     private void handleManageUsers() {
         openWindow("/pl/example/przychodniafx/ManageUsers.fxml", "Zarządzanie Użytkownikami");
-    }
-
-    @FXML
-    private void handleSearchUser() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/pl/example/przychodniafx/SearchUser.fxml"));
-            Parent root = loader.load();
-
-            SearchUserController controller = loader.getController();
-
-            // Pobieranie danych z pól tekstowych
-            String name = first_nameField.getText().trim();
-            String surname = last_nameField.getText().trim();
-
-            controller.setInitialSearchData(name, surname);
-
-            Stage stage = new Stage();
-            stage.setTitle("Wyszukaj użytkownika");
-            stage.setScene(new Scene(root, 450, 400));
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     private void openWindow(String fxmlPath, String title) {
